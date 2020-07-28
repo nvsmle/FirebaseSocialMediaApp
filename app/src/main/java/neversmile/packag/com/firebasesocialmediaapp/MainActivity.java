c51a04a7c265037da44251f5849040d9cbccb87a
nvsmle <alexandr-super@mail.ru>
2020-07-28 23:56:18 +0700

    Sign up & Sign in

--- app/src/main/java/neversmile/packag/com/firebasesocialmediaapp/MainActivity.java
+++ app/src/main/java/neversmile/packag/com/firebasesocialmediaapp/MainActivity.java
@@ -3,7 +3,10 @@ package neversmile.packag.com.firebasesocialmediaapp;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -13,6 +16,7 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -35,22 +39,20 @@ public class MainActivity extends AppCompatActivity {
 
         mAuth = FirebaseAuth.getInstance();
 
-        mAuth.createUserWithEmailAndPassword(edtEmail.getText().toString(), edtPassword.getText().toString()).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onComplete(@NonNull Task<AuthResult> task) {
-
-                if (task.isSuccessful()) {
-
-                    Toast.makeText(MainActivity.this, "Signing up Successful!", Toast.LENGTH_SHORT).show();
-
-                } else {
-
-                    Toast.makeText(MainActivity.this, "Signing up Failed!", Toast.LENGTH_SHORT).show();
-
-                }
-
+            public void onClick(View v) {
+                signUp();
             }
         });
+
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signIn();
+            }
+        });
+
 
 
     }
@@ -61,7 +63,64 @@ public class MainActivity extends AppCompatActivity {
 
         FirebaseUser currentUser = mAuth.getCurrentUser();
         if (currentUser != null) {
-            // Transition to next activity
+            transitionToSocialMediaActivity();
         }
+    }
+
+    private void signUp() {
+
+        mAuth.createUserWithEmailAndPassword(edtEmail.getText().toString(), edtPassword.getText().toString())
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+
+                if (task.isSuccessful()) {
+
+                    Toast.makeText(MainActivity.this, "Signing up Successful!", Toast.LENGTH_SHORT).show();
+
+                    FirebaseDatabase.getInstance().getReference().child("my_users")
+                            .child(task.getResult().getUser().getUid())
+                                .child("username").setValue(edtUsername.getText().toString());
+
+                    transitionToSocialMediaActivity();
+
+                } else {
+
+                    Toast.makeText(MainActivity.this, "Signing up Failed!", Toast.LENGTH_SHORT).show();
+
+                }
+
+            }
+        });
+    }
+
+    private void signIn() {
+
+        mAuth.signInWithEmailAndPassword(edtEmail.getText().toString(), edtPassword.getText().toString())
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+
+                        if (task.isSuccessful()) {
+
+                            Toast.makeText(MainActivity.this, "Signing in Successful!", Toast.LENGTH_SHORT).show();
+                            transitionToSocialMediaActivity();
+
+                        } else {
+
+                            Toast.makeText(MainActivity.this, "Signing in Failed!", Toast.LENGTH_SHORT).show();
+
+                        }
+
+                    }
+                });
+
+    }
+
+    private void transitionToSocialMediaActivity() {
+
+        Intent intent = new Intent(this, SocialMediaActivity.class);
+        startActivity(intent);
+
     }
 }
